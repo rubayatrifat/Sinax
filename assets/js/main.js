3067500e531717a13dc51018012ff350cba7abca
rubayatriat <rubayatislam17@gmail.com>
2023-01-04 00:27:34 +0600

    Sinax Agency Web

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,6 +104,30 @@
     },
   });
 
+
+  $(".testimonieal-active").owlCarousel({
+    loop: true,
+    margin: 0,
+    items: 1,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>',
+    ],
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      767: {
+        items: 1,
+      },
+      992: {
+        items: 2,
+      },
+    },
+  });
+
   /* magnificPopup img view */
   $(".popup-image").magnificPopup({
     type: "image",
